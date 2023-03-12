6f278f5dab68b6f9f920ee5070ae114fd5431eb1
csabagabor <37183688+csabagabor@users.noreply.github.com>
2023-03-12 22:58:28 +0200

    Initial version

---
+++ Jar-Parser-Utility/src/main/java/implementation/Main.java
@@ -0,0 +1,12 @@
+package implementation;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String m2Folder = "";
+        String outputPath = "";
+
+        LibraryParser.parse(m2Folder, outputPath);
+    }
+}
